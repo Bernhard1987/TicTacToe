4932e9e9a2c137123f2673217a078b1a8828175f
Bernhard1987 <demoncleaner1@gmail.com>
2023-06-06 15:12:33 +0200

    Players can now set circles and crosses after another

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let fields = [
-    'circle',
-    'cross',
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-]
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+];
+
+let currentPlayer = 'circle';
 
 function init() {
-    render();
+  render();
+}
+
+function renderField(index) {
+  const fieldValue = fields[index];
+
+  const tdElement = document.getElementById(`cell-${index}`);
+  tdElement.innerHTML = fieldValue === 'circle' ? generateCircleSVG() : generateCrossSVG();
 }
 
 function render() {
@@ -20,10 +29,9 @@ function render() {
     tableHTML += '<tr>';
     for (let j = 0; j < 3; j++) {
       let index = i * 3 + j;
-      let fieldValue = fields[index];
+      tableHTML += `<td id="cell-${index}" onclick="handleClick(${index})">`;
 
-      tableHTML += '<td onclick="handleClick(this)">';
-
+      const fieldValue = fields[index];
       if (fieldValue === 'circle') {
         tableHTML += generateCircleSVG();
       } else if (fieldValue === 'cross') {
@@ -40,51 +48,50 @@ function render() {
   contentDiv.innerHTML = tableHTML;
 }
 
-function handleClick(td) {
-  let index = Array.from(td.parentNode.children).indexOf(td);
+function handleClick(index) {
   if (fields[index] === null) {
-    if (fields.filter((field) => field !== null).length % 2 === 0) {
-      fields[index] = 'circle';
-      td.innerHTML = generateCircleSVG();
+    fields[index] = currentPlayer;
+    renderField(index);
+
+    if (currentPlayer === 'circle') {
+      currentPlayer = 'cross';
     } else {
-      fields[index] = 'cross';
-      td.innerHTML = generateCrossSVG();
+      currentPlayer = 'circle';
     }
-    td.onclick = null;
   }
 }
 
 function generateCircleSVG() {
-    const color = "#00B0EF";
-    const width = 70;
-    const height = 70;
-    const strokeWidth = 10;
-    const animationDuration = 300;
-  
-    const radius = (width - strokeWidth) / 2;
-    const circumference = 2 * Math.PI * radius;
-    const dashArray = `${circumference} ${circumference}`;
-    const dashOffset = circumference;
-  
-    const svgCode = `
+  const color = "#00B0EF";
+  const width = 70;
+  const height = 70;
+  const strokeWidth = 10;
+  const animationDuration = 300;
+
+  const radius = (width - strokeWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const dashArray = `${circumference} ${circumference}`;
+  const dashOffset = circumference;
+
+  const svgCode = `
       <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
         <circle cx="${width / 2}" cy="${height / 2}" r="${radius}" fill="none" stroke="${color}" stroke-width="${strokeWidth}" stroke-dasharray="${dashArray}" stroke-dashoffset="${dashOffset}">
           <animate attributeName="stroke-dashoffset" from="${dashOffset}" to="0" dur="${animationDuration}ms" begin="0s" fill="freeze" />
         </circle>
       </svg>
     `;
-  
-    return svgCode;
-  }
 
-  function generateCrossSVG() {
-    const color = "#FFC000";
-    const width = 70;
-    const height = 70;
-    const strokeWidth = width / 5;
-    const animationDuration = 300;
-  
-    const svgCode = `
+  return svgCode;
+}
+
+function generateCrossSVG() {
+  const color = "#FFC000";
+  const width = 70;
+  const height = 70;
+  const strokeWidth = width / 5;
+  const animationDuration = 300;
+
+  const svgCode = `
       <svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
         <line x1="0" y1="0" x2="${width}" y2="${height}" stroke="${color}" stroke-width="${strokeWidth}">
           <animate attributeName="stroke-dasharray" values="0 ${width}; ${width} 0" dur="${animationDuration}ms" begin="0s" fill="freeze" />
@@ -94,6 +101,6 @@ function generateCircleSVG() {
         </line>
       </svg>
     `;
-  
-    return svgCode;
-  }
+
+  return svgCode;
+}
